b10491ecb380cbc97e465e665c1dfba03787b4eb
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-16 21:00:06 +0700

    Rename path to relate to the project

---
+++ src/CSharp-Autokeyboard/Forms/frmMain.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace CSharpAutokeyboard
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lstWindows = new System.Windows.Forms.ListBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.tmrRefreshWindowList = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.gvKeyList = new System.Windows.Forms.DataGridView();
+            this.apply = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.keys = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.repeatKeys = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.delay = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.repeat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gvKeyList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstWindows
+            // 
+            this.lstWindows.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstWindows.FormattingEnabled = true;
+            this.lstWindows.Location = new System.Drawing.Point(13, 12);
+            this.lstWindows.Name = "lstWindows";
+            this.lstWindows.Size = new System.Drawing.Size(613, 69);
+            this.lstWindows.TabIndex = 0;
+            this.lstWindows.SelectedIndexChanged += new System.EventHandler(this.lstWindows_SelectedIndexChanged);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.Location = new System.Drawing.Point(551, 87);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // tmrRefreshWindowList
+            // 
+            this.tmrRefreshWindowList.Enabled = true;
+            this.tmrRefreshWindowList.Interval = 500;
+            this.tmrRefreshWindowList.Tick += new System.EventHandler(this.tmrRefreshWindowList_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 87);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(95, 87);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(176, 87);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save Keys";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(257, 87);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load Keys";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // gvKeyList
+            // 
+            this.gvKeyList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gvKeyList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvKeyList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.apply,
+            this.keys,
+            this.repeatKeys,
+            this.delay,
+            this.repeat});
+            this.gvKeyList.Location = new System.Drawing.Point(14, 117);
+            this.gvKeyList.Name = "gvKeyList";
+            this.gvKeyList.Size = new System.Drawing.Size(612, 136);
+            this.gvKeyList.TabIndex = 6;
+            // 
+            // apply
+            // 
+            this.apply.HeaderText = "Apply?";
+            this.apply.Name = "apply";
+            // 
+            // keys
+            // 
+            this.keys.HeaderText = "Keys";
+            this.keys.Name = "keys";
+            // 
+            // repeatKeys
+            // 
+            this.repeatKeys.HeaderText = "Repeat Keys";
+            this.repeatKeys.Name = "repeatKeys";
+            // 
+            // delay
+            // 
+            this.delay.HeaderText = "Delay";
+            this.delay.Name = "delay";
+            // 
+            // repeat
+            // 
+            this.repeat.HeaderText = "Repeat";
+            this.repeat.Name = "repeat";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 265);
+            this.Controls.Add(this.gvKeyList);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.lstWindows);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "frmMain";
+            this.Text = "Auto Keyboard";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvKeyList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstWindows;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Timer tmrRefreshWindowList;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataGridView gvKeyList;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn apply;
+        private System.Windows.Forms.DataGridViewTextBoxColumn keys;
+        private System.Windows.Forms.DataGridViewTextBoxColumn repeatKeys;
+        private System.Windows.Forms.DataGridViewTextBoxColumn delay;
+        private System.Windows.Forms.DataGridViewTextBoxColumn repeat;
+    }
+}
+
